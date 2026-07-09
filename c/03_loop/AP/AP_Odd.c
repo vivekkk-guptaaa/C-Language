cdb31a02a6f40e61df08225b64856e5a40c8bd39
vivekkk-guptaaa <vivek04ravindra@gmail.com>
2026-07-09 10:19:52 +0530

    Improved AP program comments

--- c/03_loop/AP/AP_Odd.c
+++ c/03_loop/AP/AP_Odd.c
@@ -5,7 +5,7 @@ int main()
     printf("Enter n term : ");
     scanf("%d",&n);
 
-    for(int i=1; i<=n*2-1; i=i+2){
+    for(int i=1; i<=1+(n-1)*2; i=i+2){ //an=a+(n-1)d where a=1, d=2
         printf("%d ", i);
     }
     return 0;
